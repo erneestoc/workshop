397cb6cab30f43effab7bc16d504aa2bba67f731
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2015-03-27 16:26:25 -0700

    Remove testing deps

--- mix.exs
+++ mix.exs
@@ -12,11 +12,8 @@ defmodule Chat.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: applications(Mix.env)]
+    [applications: [:logger]]
   end
-
-  defp applications(:test), do: applications(:all) ++ [:blacksmith]
-  defp applications(_all),  do: [:logger]
 
   # Dependencies can be Hex packages:
   #
@@ -28,7 +25,6 @@ defmodule Chat.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [{:shouldi, only: :test},
-     {:blacksmith, only: :test}]
+    []
   end
 end
